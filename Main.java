8909035e3734c1c79e6f347901282044a83ffcf0
rikumiyu <rikumiyu@tencent.com>
2024-04-04 12:44:00 +0800

    Init

---
+++ Main.java
@@ -0,0 +1,33 @@
+import com.google.common.hash.Hashing;
+
+public class Main {
+    public static void main(String[] args) {
+        Long seedHash;
+        System.out.println("Please enter a seed hash: ");
+        seedHash = Long.parseLong(System.console().readLine());
+        System.out.println("Seed hash: " + seedHash);
+        System.out.println("Seed: " + Main.crackSeedByHash(seedHash));
+    }
+
+    public static long crackSeedByHash(long seedHash) {
+        for (long i = 0; i < Long.MAX_VALUE; i++) {
+            long hash = Main.hashSeed(i);
+            System.out.println("Calculating Hash: " + i + " -> " + hash);
+            if (hash == seedHash) {
+                return i;
+            }
+        }
+        for (long i = 0; i >= Long.MIN_VALUE; i--) {
+            long hash = Main.hashSeed(i);
+            System.out.println("Calculating Hash: " + i + " -> " + hash);
+            if (hash == seedHash) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Seed not found!");
+    }
+
+    public static long hashSeed(long seed) {
+        return Hashing.sha256().hashLong(seed).asLong();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Minecraft Java version Seed Cracker
+
+# Usage
+
+1. Install the Bobby mod and join the world you want to crack the seed of. You can exit the game once the terrain starts to load.
+2. Inside your `.minecraft/.bobby/[server address]` folder, find the folder with the correct world data inside. The folder name is the hash of the seed.
+3. Run `java -cp guava-32.1.3-jre.jar:. Main`. When prompted, paste your hash in and hit Enter/Return.
+4. Wait until the seed has been found. (It may take up to 1 real-world day to complete on a typical Apple Silicon machine.)
