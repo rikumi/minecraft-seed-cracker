e938a160c442e0a966cd39298eff8db358fcdb70
rikumiyu <rikumiyu@tencent.com>
2024-04-04 15:06:53 +0800

    multi thread

--- Main.java
+++ Main.java
@@ -1,25 +1,46 @@
 import com.google.common.hash.Hashing;
+import java.lang.Thread;
+import java.lang.Exception;
 
 public class Main {
+    public static long shared_nextSeed = -1;
+    public static boolean shared_isFound = false;
+
     public static void main(String[] args) {
         Long seedHash;
         System.out.println("Please enter a seed hash: ");
         seedHash = Long.parseLong(System.console().readLine());
-        System.out.println("Seed hash: " + seedHash);
-        System.out.println("Seed: " + Main.crackSeedByHash(seedHash));
+
+        System.out.println("Starting from ABS: (default: 0)");
+        String startFromStr = System.console().readLine();
+        if (startFromStr.equals("")) {
+            startFromStr = "0";
+        }
+        Main.shared_nextSeed = Long.parseLong(startFromStr);
+
+        for (int i = 0; i < 8; i++) {
+            new Thread(() -> {
+                try {
+                    long seed = Main.crackSeedByHash(seedHash);
+                    Main.shared_isFound = true;
+                    System.out.println("Seed: " + seed);
+                } catch (Exception e) {}
+            }).start();
+        }
     }
 
     public static long crackSeedByHash(long seedHash) {
-        for (long i = 0; i < Long.MAX_VALUE; i++) {
-            long hash = Main.hashSeed(i);
-            System.out.println("Calculating Hash: +" + i + " -> " + hash);
+        while (!Main.shared_isFound) {
+            long seed = Main.shared_nextSeed++;
+            long hash = Main.hashSeed(seed);
+            System.out.println("Calculating Hash: +" + seed + " -> " + hash);
             if (hash == seedHash) {
-                return i;
+                return seed;
             }
-            long hashNeg = Main.hashSeed(-i);
-            System.out.println("Calculating Hash: -" + i + " -> " + hash);
+            long hashNeg = Main.hashSeed(-seed);
+            System.out.println("Calculating Hash: -" + seed + " -> " + hash);
             if (hash == seedHash) {
-                return -i;
+                return -seed;
             }
         }
         throw new RuntimeException("Seed not found!");
